944e0b5caacf40c29d9995dc6704357de9a69ae1
archirick <48961843+archirick@users.noreply.github.com>
2019-04-05 18:24:45 +0530

    Add files via upload

---
+++ multi dimentional list.py
@@ -0,0 +1,13 @@
+
+List=["Python","For","Game"]
+print("Accessing a element from the list")
+print(List[0])
+print(List[2])
+
+
+out put
+
+List=[['Python','For'],['AI']]
+print("Accessing a element from a multi-Dimentional list")
+print(List[0][1])
+print(List[1][0])
